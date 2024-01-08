a70176b7a175167eb93bfda0a084473e9d4fdbdc
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2024-01-08 20:16:17 +0100

    try react chrono

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import "./App.css";
+import { Chrono } from "react-chrono";
 
 function App() {
-  return <div>app</div>;
+  const items = [
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "http://someurl/image.jpg",
+        },
+      },
+    },
+    {
+      title: "May 1939",
+      cardTitle: "Dunkirkddd",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "http://someurl/image.jpg",
+        },
+      },
+    },
+  ];
+
+  return (
+    <div>
+      <Chrono items={items} mode="VERTICAL_ALTERNATING" />
+    </div>
+  );
 }
 
 export default App;
