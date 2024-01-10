68a5923e9f9e8861fb2e3bbffb6e7c62325179aa
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2024-01-10 15:58:21 +0100

    add timeline json file

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
 import "./App.css";
 import { Chrono } from "react-chrono";
+import items from "./timeline.json";
 
 function App() {
-  const items = [
-    {
-      title: "May 1940",
-      cardTitle: "Dunkirk",
-      url: "http://www.history.com",
-      cardSubtitle:
-        "Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText:
-        "Men of the British Expeditionary Force (BEF) wade out to..",
-      media: {
-        type: "IMAGE",
-        source: {
-          url: "http://someurl/image.jpg",
-        },
-      },
-    },
-    {
-      title: "May 1939",
-      cardTitle: "Dunkirkddd",
-      url: "http://www.history.com",
-      cardSubtitle:
-        "Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText:
-        "Men of the British Expeditionary Force (BEF) wade out to..",
-      media: {
-        type: "IMAGE",
-        source: {
-          url: "http://someurl/image.jpg",
-        },
-      },
-    },
-  ];
-
   return (
     <div>
-      <Chrono items={items} mode="VERTICAL_ALTERNATING" />
+      <Chrono enableDarkToggle={true} slideShow={true} items={items} mode="VERTICAL_ALTERNATING" />
     </div>
   );
 }
