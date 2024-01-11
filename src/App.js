b4909dc18805b6994cf0ccc4eadc47fdd51cd73e
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2024-01-11 10:58:32 +0100

    edit timeline json file and add h1 title in app.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import items from "./timeline.json";
 
 function App() {
   return (
-    <div>
-      <Chrono enableDarkToggle={true} slideShow={true} items={items} mode="VERTICAL_ALTERNATING" />
-    </div>
+    <>
+      <h1 style={{"textAlign": "center"}}>Les grandes dates de l'Histoire de France</h1>
+      <div>
+        <Chrono items={items} mediaHeight={300} mode="HORIZONTAL" />
+        {/* <Chrono items={items} mediaHeight={300}  /> */}
+      </div>
+    </>
   );
 }
 
